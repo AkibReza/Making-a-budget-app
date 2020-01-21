8ebc4ba69d155ca182643167188eefcf389bfe15
AkibReza <akib.reza23@gmail.com>
2020-01-21 14:00:43 +0600

    Commit before making new item appear in UI

--- app.js
+++ app.js
@@ -1,25 +1,28 @@
 //Inner work
 var budgetController = (function () {
 
-    //Function constructor for each and every entry of Expense
-    var Expense = function(id, description, value){
+    //Function constructor for each and every item of Expense
+    var Expense = function (id, description, value) {
         this.id = id;
         this.description = description;
-        this.value  = value; 
+        this.value = value;
     };
-    //Function constructor for each and every entry of Income
-    var Income = function(id, description, value){
+
+    //Function constructor for each and every item of Income
+    var Income = function (id, description, value) {
         this.id = id;
         this.description = description;
-        this.value  = value; 
+        this.value = value;
     };
-    //Storing all the entries into one place
+
+    //Storing all the items into one place
     var allExpenses = [];
     var allIncome = [];
+
     //Starting value = 0;
     var totalExpenses = 0;
 
-    
+    // Storing all the values into one place (? not sure)
     var data = {
         allItems: {
             exp: [],
@@ -29,7 +32,32 @@ var budgetController = (function () {
             exp: 0,
             inc: 0
         }
-    }
+    };
+
+    return {
+        addItem: function (tip, des, val) {
+            var newItem, ID;
+
+            //Create new ID
+            if (data.allItems[type].length > 0) {
+                ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+            } else {
+                ID = 0;
+            }
+
+
+            //Create new item depending on exp or inc
+            if (type === 'exp') {
+                newItem = new Expense(ID, des, val);
+            } else if (type === 'inc') {
+                newItem = new Income(ID, des, val);
+            }
+            //Add the new item into the array
+            data.allItems[type].push(newItem);
+
+            return newItem;
+        }
+    };
 
 
 
@@ -53,6 +81,9 @@ var UIcontroller = (function () {
                 value: document.querySelector(StringLibrary.inputValue).value //Amount
             };
         },
+
+        
+
         //Using this method, StringLibrary can now be used outside of the function 
         getStringLibrary: function () {
             return StringLibrary;
@@ -64,7 +95,7 @@ var UIcontroller = (function () {
 
 //Handles input
 var controller = (function (budgetCtrl, UIctrl) {
-    
+
     //Keeping all the event listeners in one place
 
     var setupEventListeners = function () {
@@ -85,12 +116,14 @@ var controller = (function (budgetCtrl, UIctrl) {
     };
 
     var itemAdder = function () {
+        var input, newItem;
         // 1. Get the filed input data.
 
-        var input = UIctrl.getInput();
-        
+        input = UIctrl.getInput();
 
         // 2. Add the item to the inner work controller.
+
+        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
         // 3. Add the item to the Display controller.
 
@@ -99,14 +132,14 @@ var controller = (function (budgetCtrl, UIctrl) {
         // 5. Show the budget to the display.
 
     }
-        /*The above event handler won't get executed because it's not an IIFE
-        So the best choice is to create an INITIALIZATION FUNCTION*/
-        return {
-            init: function(){
-                console.log("Nicely started")
-                setupEventListeners();
-            }
-        };
+    /*The above event handler won't get executed because it's not an IIFE
+    So the best choice is to create an INITIALIZATION FUNCTION*/
+    return {
+        init: function () {
+            console.log("Nicely started")
+            setupEventListeners();
+        }
+    };
 
 })(budgetController, UIcontroller);
 
